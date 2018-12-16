7e32caf86ccfee599e3485845c1ec450cc7aaa19
Erik Unger <erik@erikunger.com>
2018-12-16 17:58:31 +0100

    replaced RandStringOfSize with RandomString

--- strutil/string.go
+++ strutil/string.go
@@ -1,9 +1,6 @@
 package strutil
 
 import (
-	"math/rand"
-	"time"
-
 	"github.com/guregu/null"
 )
 
@@ -75,33 +72,6 @@ func IndexInStrings(str string, slice []string) int {
 		}
 	}
 	return -1
-}
-
-const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const (
-	letterIdxBits = 6                    // 6 bits to represent a letter index
-	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
-	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
-)
-
-func RandStringOfSize(n int) string {
-	var src = rand.NewSource(time.Now().UnixNano())
-
-	b := make([]byte, n)
-	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
-	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
-		if remain == 0 {
-			cache, remain = src.Int63(), letterIdxMax
-		}
-		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
-			b[i] = letterBytes[idx]
-			i--
-		}
-		cache >>= letterIdxBits
-		remain--
-	}
-
-	return string(b)
 }
 
 func Truncate(s string, i int) string {
