e1a10990fe80a0043d0e2e4227895d0a56d10c55
Erik Unger <erik@erikunger.com>
2019-01-11 18:00:22 +0100

    more StringSet methods

--- strutil/stringset.go
+++ strutil/stringset.go
@@ -1,6 +1,9 @@
 package strutil
 
-import "sort"
+import (
+	"sort"
+	"strings"
+)
 
 type StringSet map[string]struct{}
 
@@ -21,4 +24,72 @@ func (set StringSet) Sorted() (s []string) {
 		sort.Strings(s)
 	}
 	return s
+}
+
+func (set StringSet) String() string {
+	return "[" + strings.Join(set.Sorted(), ",") + "]"
+}
+
+func (set StringSet) AddSlice(s []string) {
+	for _, str := range s {
+		set[str] = struct{}{}
+	}
+}
+
+func (set StringSet) AddSet(other StringSet) {
+	for str := range other {
+		set[str] = struct{}{}
+	}
+}
+
+func (set StringSet) Add(str string) {
+	set[str] = struct{}{}
+}
+
+func (set StringSet) Contains(str string) bool {
+	_, has := set[str]
+	return has
+}
+
+func (set StringSet) Delete(str string) {
+	delete(set, str)
+}
+
+func (set StringSet) DeleteAll() {
+	for str := range set {
+		delete(set, str)
+	}
+}
+
+func (set StringSet) DeleteSlice(s []string) {
+	for _, str := range s {
+		delete(set, str)
+	}
+}
+
+func (set StringSet) DeleteSet(other StringSet) {
+	for str := range other {
+		delete(set, str)
+	}
+}
+
+func (set StringSet) Clone() StringSet {
+	clone := make(StringSet)
+	clone.AddSet(set)
+	return clone
+}
+
+func (set StringSet) Diff(other StringSet) StringSet {
+	diff := make(StringSet)
+	for str := range set {
+		if !other.Contains(str) {
+			diff.Add(str)
+		}
+	}
+	for str := range other {
+		if !set.Contains(str) {
+			diff.Add(str)
+		}
+	}
+	return diff
 }
