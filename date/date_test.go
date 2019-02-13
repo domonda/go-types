97c583f667782440ad5df23e5ef70e02c944a6ae
Erik Unger <erik@erikunger.com>
2019-02-13 15:57:49 +0100

    added more date tests yet to pass

--- date/date_test.go
+++ date/date_test.go
@@ -8,39 +8,159 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var dateTable = []string{
-	"2006-01-02", "2006-01-02",
-	"2006/01/02", "2006-01-02",
-	"2006.01.02", "2006-01-02",
-	"2006 01 02", "2006-01-02",
+var dateTable = map[string]Date{
+	"2006-01-02": "2006-01-02",
+	"2006/01/02": "2006-01-02",
+	"2006.01.02": "2006-01-02",
+	"2006 01 02": "2006-01-02",
 
-	"25.12.1975", "1975-12-25",
-	"25.12.75", "1975-12-25",
+	"25.12.1975": "1975-12-25",
+	"25.12.75":   "1975-12-25",
 
-	"12/25/1975", "1975-12-25",
-	"12/25/75", "1975-12-25",
+	"12/25/1975": "1975-12-25",
+	"12/25/75":   "1975-12-25",
 
-	"01.02.03", "2003-02-01",
-	// "1.2.03", "2003-02-01", // too short
-	"1.2.2003", "2003-02-01",
+	"01.02.03": "2003-02-01",
+	// "1.2.03": "2003-02-01", // too short
+	"1.2.2003": "2003-02-01",
 
-	"1st of 02/2003", "2003-02-01",
-	"4th of 02/2003", "2003-02-04",
+	"1st of 02/2003": "2003-02-01",
+	"4th of 02/2003": "2003-02-04",
 
-	"jan. 24 2012", "2012-01-24",
-	"Februar 24 89", "1989-02-24",
-	"February 3rd 89", "1989-02-03",
+	"jan. 24 2012":    "2012-01-24",
+	"Februar 24 89":   "1989-02-24",
+	"February 3rd 89": "1989-02-03",
 
-	"1. Dezember 2016", "2016-12-01",
-	"1st of dec. 2020", "2020-12-01",
+	"1. Dezember 2016": "2016-12-01",
+	"1st of dec. 2020": "2020-12-01",
 
-	"2016 Dezember 9th", "2016-12-09",
-	"16 Dezember 9th", "2016-12-09",
-	"16. Dezember 98", "1998-12-16",
-	"16th of Dec. 04", "2004-12-16",
+	"2016 Dezember 9th": "2016-12-09",
+	"16 Dezember 9th":   "2016-12-09",
+	"16. Dezember 98":   "1998-12-16",
+	"16th of Dec. 04":   "2004-12-16",
 
-	"2016 25. März", "2016-03-25",
-	"75 1st of march", "1975-03-01",
+	"2016 25. März":   "2016-03-25",
+	"75 1st of march": "1975-03-01",
+
+	// TODO
+	// "2 janv. 2019",  // french
+	// "30 janv. 2019", // french
+	// "23/gen/2019",   // italian
+
+	// Test data from https://raw.githubusercontent.com/araddon/dateparse/master/parseany_test.go
+	// "oct 7, 1970":   "1970-10-07", // TODO
+	// "oct 7, '70":    "1970-10-07", // TODO
+	// "Oct 7, '70":    "1970-10-07", // TODO
+	// "Oct. 7, '70":   "1970-10-07", // TODO
+	// "oct. 7, '70":   "1970-10-07", // TODO
+	// "oct. 7, 1970": "1970-10-07", // TODO
+	// "Sept. 7, '70":  "1970-09-07", // TODO
+	// "sept. 7, 1970": "1970-09-07", // TODO
+	// "Feb 8, 2009":      "2009-02-08", // TODO
+	"7 oct 70":         "1970-10-07",
+	"7 oct 1970":       "1970-10-07",
+	"7 May 1970":       "1970-05-07",
+	"7 Sep 1970":       "1970-09-07",
+	"7 June 1970":      "1970-06-07",
+	"7 September 1970": "1970-09-07",
+	// RubyDate    = "Mon Jan 02 15:04:05 -0700 2006"
+	// "Mon Jan 02 2006": "2006-01-02", // TODO
+	// "Thu May 08 2009": "2009-05-08", // TODO
+	// Month dd, yyyy at time
+	// "September 17, 2012": "2012-09-17", // TODO
+	// "May 17, 2012":       "2012-05-17", // TODO
+	// Month dd yyyy time
+	// "September 17 2012": "2012-09-17", // TODO
+	// Month dd, yyyy
+	// "May 7, 2012":  "2012-05-07", // TODO
+	// "June 7, 2012": "2012-06-07", // TODO
+	// "June 7 2012":  "2012-06-07", // TODO
+	// Month dd[th,nd,st,rd] yyyy
+	// "September 17th, 2012": "2012-09-17", // TODO
+	// "September 17th 2012":  "2012-09-17", // TODO
+	// "September 7th, 2012":  "2012-09-07", // TODO
+	// "September 7th 2012":   "2012-09-07", // TODO
+	// "May 1st 2012":         "2012-05-01", // TODO
+	// "May 1st, 2012":        "2012-05-01", // TODO
+	// "May 21st 2012":        "2012-05-21", // TODO
+	// "May 21st, 2012":       "2012-05-21", // TODO
+	// "May 23rd 2012":        "2012-05-23", // TODO
+	// "May 23rd, 2012":       "2012-05-23", // TODO
+	// "June 2nd, 2012":       "2012-06-02", // TODO
+	// "June 2nd 2012":        "2012-06-02", // TODO
+	// "June 22nd, 2012":      "2012-06-22", // TODO
+	// "June 22nd 2012":       "2012-06-22", // TODO
+	// ?
+	// "Fri, 03 Jul 2015": "2015-07-03", // TODO
+	// "Fri, 3 Jul 2015":  "2015-07-03", // TODO
+	// "Thu, 03 Jul 2017": "2017-07-03", // TODO
+	// "Thu, 3 Jul 2017":  "2017-07-03", // TODO
+	// "Tue, 11 Jul 2017": "2017-07-11", // TODO
+	// "Tue, 5 Jul 2017":  "2017-07-05", // TODO
+	// "Fri, 03-Jul-15":   "2015-07-03", // TODO
+	// "Fri, 03-Jul 2015": "2015-07-03", // TODO
+	// "Fri, 3-Jul-15":    "2015-07-03", // TODO
+	// RFC850    = "Monday, 02-Jan-06 15:04:05 MST"
+	// "Wednesday, 07-May-09": "2009-05-07", // TODO
+	// "Wednesday, 28-Feb-18": "2018-02-28", // TODO
+	// with offset then with variations on non-zero filled stuff
+	// "Monday, 02 Jan 2006":    "2006-01-02", // TODO
+	// "Wednesday, 28 Feb 2018": "2018-02-28", // TODO
+	// "Wednesday, 2 Feb 2018":  "2018-02-02", // TODO
+	//  dd mon yyyy  12 Feb 2006, 19:17:08
+	"07 Feb 2004": "2004-02-07",
+	"7 Feb 2004":  "2004-02-07",
+	//  dd-mon-yyyy   12-Feb-2006 19:17:08
+	"07-Feb-2004": "2004-02-07",
+	//  dd-mon-yy   12-Feb-2006 19:17:08
+	"07-Feb-04": "2004-02-07",
+	// yyyy-mon-dd    2013-Feb-03
+	"2013-Feb-03": "2013-02-03",
+	// 03 February 2013
+	"03 February 2013": "2013-02-03",
+	"3 February 2013":  "2013-02-03",
+	// Chinese 2014年04月18日
+	// "2014年04月08日": "2014-04-08", // TODO
+	//  mm/dd/yyyy
+	"03/31/2014": "2014-03-31",
+	"3/31/2014":  "2014-03-31",
+	//  mm/dd/yy
+	"08/08/71": "1971-08-08",
+	// "8/8/71":   "1971-08-08", // TODO
+	//   yyyy/mm/dd
+	"2014/04/02": "2014-04-02",
+	"2014/03/31": "2014-03-31",
+	"2014/4/2":   "2014-04-02",
+	//   yyyy-mm-dd
+	"2014-04-02": "2014-04-02",
+	"2014-03-31": "2014-03-31",
+	"2014-4-2":   "2014-04-02",
+	// yyyy-mm
+	// "2014-04": "2014-04-01", // TODO
+	//   yyyy-mm-dd hh:mm:ss AM
+	"2014-04-26": "2014-04-26",
+	//   yyyy-mm-dd hh:mm:ss,000
+	"2014-05-11": "2014-05-11",
+	//   yyyy-mm-dd hh:mm:ss +0000
+	"2012-08-03": "2012-08-03",
+	"2012-8-03":  "2012-08-03",
+	"2012-8-3":   "2012-08-03",
+
+	"2014-05-01": "2014-05-01",
+	"2014-5-01":  "2014-05-01",
+	"2014-05-1":  "2014-05-01",
+	// yyyy.mm
+	// "2014.05": "2014-05-01", // TODO
+	//   mm.dd.yyyy
+	"3.31.2014":  "2014-03-31",
+	"3.3.2014":   "2014-03-03",
+	"03.31.2014": "2014-03-31",
+	//   mm.dd.yy
+	"08.21.71": "1971-08-21",
+	//  yyyymmdd and similar
+	// "2014":     "2014-01-01", // TODO
+	// "201412":   "2014-12-01", // TODO
+	// "20140601": "2014-06-01", // TODO
 }
 
 var invalidDates = []string{
@@ -50,21 +170,21 @@ var invalidDates = []string{
 }
 
 func Test_Normalize(t *testing.T) {
-	for i := 0; i < len(dateTable); i += 2 {
-		normalized, err := Normalize(dateTable[i])
+	for input, expected := range dateTable {
+		normalized, err := Normalize(input)
 		if err != nil {
-			t.Errorf("Normalize(%s): %s", dateTable[i], err.Error())
+			t.Errorf("Normalize(%#v): %s", input, err.Error())
 			continue
 		}
-		if string(normalized) != dateTable[i+1] {
-			t.Errorf("Normalize(%s): %s != %s", dateTable[i], normalized, dateTable[i+1])
+		if normalized != expected {
+			t.Errorf("Normalize(%#v): %#v != %#v", input, normalized, expected)
 		}
 	}
 
 	for _, invalidDate := range invalidDates {
 		normalized, err := Normalize(invalidDate)
 		if err == nil {
-			t.Errorf("Should NOT be valid Normalize(%s): %s", invalidDate, normalized)
+			t.Errorf("Should NOT be valid Normalize(%#v): %#v", invalidDate, normalized)
 		}
 	}
 }
@@ -87,20 +207,20 @@ func Test_Finder(t *testing.T) {
 			for _, indices := range allResult {
 				fmt.Println("'" + str[indices[0]:indices[1]] + "'")
 			}
-			t.Errorf("Found %d Dates in '%s', but expected %d", len(allResult), str, len(allIndices))
+			t.Errorf("Found %d Dates in %#v, but expected %d", len(allResult), str, len(allIndices))
 		} else {
 			for i := range allIndices {
 				indices := allIndices[i]
 				result := allResult[i]
 				if len(result) != 2 {
-					t.Errorf("Did not find date in '%s'", str)
+					t.Errorf("Did not find date in %#v", str)
 				}
 				date := Date(str[result[0]:result[1]])
 				if result[0] != indices[0] || result[1] != indices[1] {
-					t.Errorf("Found Date '%s' at wrong position in '%s'. Expected: %v, Result: %v", date, str, indices, result)
+					t.Errorf("Found Date %#v at wrong position in %#v. Expected: %v, Result: %v", date, str, indices, result)
 				}
 				if !date.Valid() {
-					t.Errorf("Invalid Date: %s", date)
+					t.Errorf("Invalid Date: %#v", date)
 				}
 			}
 		}
