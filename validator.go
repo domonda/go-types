88c67ef547767f6474f4ec370d7631dace6b554c
Erik Unger <erik@erikunger.com>
2019-02-14 18:02:28 +0100

    added Validate() and ValidatorAsValidatErr

--- validator.go
+++ validator.go
@@ -1,5 +1,7 @@
 package types
 
+import "errors"
+
 // Validator can be implemented by types that can validate their data.
 type Validator interface {
 	// Valid returns if the data of the implementation is valid.
@@ -10,4 +12,39 @@ type Validator interface {
 type ValidatErr interface {
 	// Validate returns an error if the data of the implementation is not valid.
 	Validate() error
+}
+
+// ValidatorAsValidatErr wraps a Validator as a ValidatErr,
+// returning ErrInvalidValue when Validator.Valid() returns false.
+type ValidatorAsValidatErr struct {
+	Validator
+}
+
+func (v ValidatorAsValidatErr) Validate() error {
+	if v.Valid() {
+		return nil
+	}
+	return ErrInvalidValue
+}
+
+// ErrInvalidValue means that a value is not valid,
+// returned by Validate() and ValidatorAsValidatErr.Validate().
+var ErrInvalidValue = errors.New("invalid value")
+
+// Validate returns an error if v implements ValidatErr or Validator
+// and the methods ValidatErr.Validate() or Validator.Valid()
+// indicate an invalid value.
+// The error from ValidatErr.Validate() is returned directly,
+// and ErrInvalidValue is returned if Validator.Valid() is false.
+// If v does not implement ValidatErr or Validator then nil will be returned.
+func Validate(v interface{}) error {
+	switch x := v.(type) {
+	case ValidatErr:
+		return x.Validate()
+	case Validator:
+		if !x.Valid() {
+			return ErrInvalidValue
+		}
+	}
+	return nil
 }
