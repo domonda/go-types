144af6b3a8ce58af621caf8ef3d4faf4bc623a24
Erik Unger <erik@erikunger.com>
2019-03-11 17:03:37 +0100

    propper implementation of vat.NullableID, ID.Scan and  Value now return error for NULL/empty string

--- vat/id.go
+++ vat/id.go
@@ -11,7 +11,8 @@ import (
 
 // ID is a european VAT ID.
 // ID implements the database/sql.Scanner and database/sql/driver.Valuer interfaces,
-// and will treat an empty ID string as SQL NULL value.
+// returning errors when the ID is not valid and can't be normalized.
+// Use NullableID to read and write SQL NULL values.
 type ID string
 
 // NormalizeVATID returns str as normalized VAT ID or an error.
@@ -48,6 +49,99 @@ func isVATIDTrimRune(r rune) bool {
 	return unicode.IsPunct(r)
 }
 
+// Normalized returns the id in normalized form,
+// or an error if the VAT ID is not valid.
+func (id ID) Normalized() (ID, error) {
+	if len(id) < IDMinLength {
+		return "", errors.Errorf("VAT ID '%s' is too short", id)
+	}
+	countryCode := country.Code(id[:2])
+	regex, found := vatidRegex[countryCode]
+	if !found {
+		return "", errors.Errorf("VAT ID '%s' has an invalid country code: '%s'", id, countryCode)
+	}
+	normalized := strutil.RemoveRunesString(string(id), unicode.IsSpace)
+	if !regex.MatchString(normalized) {
+		return "", errors.Errorf("VAT ID '%s' has an invalid format", id)
+	}
+	check, found := vatidCheckSum[countryCode]
+	if found && !check(normalized) {
+		return "", errors.Errorf("VAT ID '%s' has an invalid check-sum", id)
+	}
+	return ID(normalized), nil
+}
+
+// NormalizedOrNull returns the id in normalized form
+// or Null if the VAT ID is not valid.
+func (id ID) NormalizedOrNull() NullableID {
+	normalized, err := id.Normalized()
+	if err != nil {
+		return Null
+	}
+	return NullableID(normalized)
+}
+
+// Valid returns if id is a valid VAT ID,
+// ignoring normalization.
+func (id ID) Valid() bool {
+	_, err := id.Normalized()
+	return err == nil
+}
+
+// ValidAndNormalized returns if id is a valid and normalized VAT ID.
+func (id ID) ValidAndNormalized() bool {
+	norm, err := id.Normalized()
+	return err == nil && id == norm
+}
+
+// Validate returns an error if id is not a valid VAT ID,
+// ignoring normalization.
+func (id ID) Validate() error {
+	_, err := id.Normalized()
+	return err
+}
+
+// ValidateIsNormalized returns an error if id is not a valid and normalized VAT ID.
+func (id ID) ValidateIsNormalized() error {
+	norm, err := id.Normalized()
+	if err != nil {
+		return err
+	}
+	if id != norm {
+		return errors.Errorf("VAT ID is valid but not normalized: '%s'", id)
+	}
+	return nil
+}
+
+// NullableID returns the id as NullableID
+func (id ID) NullableID() NullableID {
+	return NullableID(id)
+}
+
+// CountryCode returns the country.Code of the VAT ID,
+// or country.Null if the id is not valid.
+func (id ID) CountryCode() country.Code {
+	norm, err := id.Normalized()
+	if err != nil {
+		return country.Null
+	}
+	code := country.Code(norm[:2])
+	if code == "EL" {
+		return "GR"
+	}
+	return code
+}
+
+// Number returns the number part after the country code of the VAT ID,
+// or and empty string if the id is not valid.
+func (id ID) Number() string {
+	norm, err := id.Normalized()
+	if err != nil {
+		return ""
+	}
+	return string(norm[2:])
+}
+
 // AssignString tries to parse and assign the passed
 // source string as value of the implementing object.
 // It returns an error if source could not be parsed.
@@ -61,110 +155,34 @@ func (id *ID) AssignString(source string) (normalized bool, err error) {
 		return false, err
 	}
 	*id = newID
-	return newID == ID(source), nil
+	return string(newID) == source, nil
 }
 
 // Scan implements the database/sql.Scanner interface.
 func (id *ID) Scan(value interface{}) error {
+	var newID ID
 	switch x := value.(type) {
 	case string:
-		*id = ID(x)
+		newID = ID(x)
 	case []byte:
-		*id = ID(x)
+		newID = ID(x)
 	case nil:
-		*id = Null.ID
+		return errors.New("can't scan SQL NULL as vat.ID")
 	default:
 		return errors.Errorf("can't scan SQL value of type %T as vat.ID", value)
 	}
+	newID, err := newID.Normalized()
+	if err != nil {
+		return err
+	}
+	*id = newID
 	return nil
 }
 
 // Value implements the driver database/sql/driver.Valuer interface.
 func (id ID) Value() (driver.Value, error) {
-	if id == Null.ID {
-		return nil, nil
-	}
-	return string(id), nil
-}
-
-func (id ID) Valid() bool {
-	_, err := id.Normalized()
-	return err == nil
-}
-
-func (id ID) Validate() error {
-	_, err := id.Normalized()
-	return err
-}
-
-// ValidateNormalized returns an error if the id is not valid,
-// or valid but not normalized.
-func (id ID) ValidateNormalized() error {
 	norm, err := id.Normalized()
-	if err != nil {
-		return err
-	}
-	if id != norm {
-		return errors.Errorf("VAT ID valid but not normalized: '%s'", id)
-	}
-	return nil
-}
-
-func (id ID) ValidAndNormalized() bool {
-	norm, err := id.Normalized()
-	return err == nil && id == norm
-}
-
-func (id ID) NullableID() NullableID {
-	return NullableID{id}
-}
-
-func (id ID) CountryCode() country.Code {
-	if !id.Valid() {
-		return country.Null
-	}
-	code := country.Code(id[:2])
-	if code == "EL" {
-		return "GR"
-	}
-	return code
-}
-
-func (id ID) Number() string {
-	if !id.Valid() {
-		return ""
-	}
-	return string(id[2:])
-}
-
-func (id ID) Normalized() (ID, error) {
-	if len(id) < IDMinLength {
-		return "", errors.New("VAT ID is too short")
-	}
-	countryCode := country.Code(id[:2])
-	regex, found := vatidRegex[countryCode]
-	if !found {
-		return "", errors.New("invalid VAT ID country code: " + string(countryCode))
-	}
-	normalized := strutil.RemoveRunesString(string(id), unicode.IsSpace)
-	if !regex.MatchString(normalized) {
-		return "", errors.New("invalid VAT ID format")
-	}
-	check, found := vatidCheckSum[countryCode]
-	if found && !check(normalized) {
-		return "", errors.New("invalid VAT ID check-sum")
-	}
-	return ID(normalized), nil
-}
-
-// NormalizedOrNull returns id in normalized form
-// or Null if id is not valid.
-func (id ID) NormalizedOrNull() NullableID {
-	normalized, err := id.Normalized()
-	if err != nil {
-		return Null
-	}
-	return NullableID{normalized}
+	return string(norm), err
 }
 
 func vatidCheckSumAT(id string) bool {
--- vat/nullableid.go
+++ vat/nullableid.go
@@ -1,25 +1,147 @@
 package vat
 
+import (
+	"database/sql/driver"
+
+	"github.com/domonda/errors"
+	"github.com/domonda/go-types/country"
+)
+
 // Null is an empty string and will be treatet as SQL NULL.
 var Null NullableID
 
 // NullableID is identical to ID, except that the Null value (empty string)
 // is considered valid by the Valid() and Validate() methods.
-type NullableID struct {
-	ID
+type NullableID string
+
+// Normalized returns the id in normalized form,
+// or an error if the VAT ID is not Null and not valid.
+func (n NullableID) Normalized() (NullableID, error) {
+	if n == Null {
+		return Null, nil
+	}
+	id, err := ID(n).Normalized()
+	return NullableID(id), err
 }
 
+// NormalizedOrNull returns n in normalized form
+// or Null if id is not valid.
+func (n NullableID) NormalizedOrNull() NullableID {
+	normalized, err := n.Normalized()
+	if err != nil {
+		return Null
+	}
+	return normalized
+}
+
+// NormalizedNotNull returns the id in normalized form,
+// or an error if the VAT ID is not valid or Null.
+func (n NullableID) NormalizedNotNull() (ID, error) {
+	return ID(n).Normalized()
+}
+
+// Valid returns if id is a valid VAT ID or Null,
+// ignoring normalization.
 func (n NullableID) Valid() bool {
 	return n.Validate() == nil
 }
 
-func (n NullableID) Validate() error {
-	if n.ID == "" {
-		return nil
-	}
-	return n.ID.Validate()
+// ValidAndNormalized returns if id is Null or a valid and normalized VAT ID.
+func (n NullableID) ValidAndNormalized() bool {
+	norm, err := n.Normalized()
+	return err == nil && n == norm
 }
 
-func (n NullableID) String() string {
-	return string(n.ID)
+// ValidAndNotNull returns if this is a valid not Null VAIT ID.
+func (n NullableID) ValidAndNotNull() bool {
+	return n != Null && n.Valid()
+}
+
+// Validate returns an error if id is not a valid VAT ID or Null,
+// ignoring normalization.
+func (n NullableID) Validate() error {
+	if n == Null {
+		return nil
+	}
+	return ID(n).Validate()
+}
+
+// ValidateIsNormalized returns an error if id is not Null or a valid and normalized VAT ID.
+func (n NullableID) ValidateIsNormalized() error {
+	norm, err := n.Normalized()
+	if err != nil {
+		return err
+	}
+	if n != norm {
+		return errors.Errorf("VAT ID is valid but not normalized: '%s'", n)
+	}
+	return nil
+}
+
+// ValidateIsNormalizedAndNotNull returns an error if id is not a valid and normalized VAT ID.
+func (n NullableID) ValidateIsNormalizedAndNotNull() error {
+	return ID(n).ValidateIsNormalized()
+}
+
+// ID returns the NullableID as ID without any validation.
+func (n NullableID) ID() ID {
+	return ID(n)
+}
+
+// CountryCode returns the country.Code of the VAT ID,
+// or country.Null if the id is not valid.
+func (n NullableID) CountryCode() country.Code {
+	return ID(n).CountryCode()
+}
+
+// Number returns the number part after the country code of the VAT ID,
+// or and empty string if the id is not valid.
+func (n NullableID) Number() string {
+	return ID(n).Number()
+}
+
+// AssignString tries to parse and assign the passed
+// source string as value of the implementing object.
+// It returns an error if source could not be parsed.
+// If the source string could be parsed, but was not
+// in the expected normalized format, then false is
+// returned for normalized and nil for err.
+// AssignString implements strfmt.StringAssignable
+func (n *NullableID) AssignString(source string) (normalized bool, err error) {
+	newID, err := NullableID(source).Normalized()
+	if err != nil {
+		return false, err
+	}
+	*n = newID
+	return string(newID) == source, nil
+}
+
+// Scan implements the database/sql.Scanner interface.
+func (n *NullableID) Scan(value interface{}) error {
+	var newID NullableID
+	switch x := value.(type) {
+	case string:
+		newID = NullableID(x)
+	case []byte:
+		newID = NullableID(x)
+	case nil:
+		// newID = Null
+	default:
+		return errors.Errorf("can't scan SQL value of type %T as vat.NullableID", value)
+	}
+	newID, err := newID.Normalized()
+	if err != nil {
+		return err
+	}
+	*n = newID
+	return nil
+}
+
+// Value implements the driver database/sql/driver.Valuer interface.
+func (n NullableID) Value() (driver.Value, error) {
+	if n == Null {
+		return nil, nil
+	}
+	norm, err := ID(n).Normalized()
+	return string(norm), err
 }
