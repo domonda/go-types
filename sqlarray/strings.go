cc283ad1398494e6d39dfaa85340d5a3aaa65d7c
Erik Unger <erik@erikunger.com>
2019-04-06 12:52:38 +0200

    added sqlarray.StringsNotNull

--- sqlarray/strings.go
+++ sqlarray/strings.go
@@ -1,6 +1,8 @@
 package sqlarray
 
 import (
+	"database/sql/driver"
+
 	"github.com/lib/pq"
 )
 
@@ -9,3 +11,19 @@ import (
 // A nil slice is mapped to the SQL NULL value,
 // and a non nil zero length slice to an empty SQL array '{}'.
 type Strings = pq.StringArray
+
+// StringsNotNull always returns an non NULL SQL array, even if the slice is nil.
+type StringsNotNull []string
+
+// Scan implements the sql.Scanner interface.
+func (a *StringsNotNull) Scan(src interface{}) error {
+	return (*pq.StringArray)(a).Scan(src)
+}
+
+// Value implements the driver.Valuer interface.
+func (a StringsNotNull) Value() (driver.Value, error) {
+	if len(a) == 0 {
+		return "{}", nil
+	}
+	return pq.StringArray(a).Value()
+}
