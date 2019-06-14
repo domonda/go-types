1360922696ce3825ca80aed8d6bebed40111e13d
Denis Badurina <badurinadenis@gmail.com>
2019-06-14 21:19:09 +0200

    strutil: StrMutex with wait group

--- strutil/strmutex.go
+++ strutil/strmutex.go
@@ -8,34 +8,54 @@ import (
 // StrMutex allows mutex locking per UUID
 type StrMutex struct {
 	mapMutex   sync.Mutex
-	strMutexes map[string]*sync.Mutex
+	strMutexes map[string]struct {
+		mtx *sync.Mutex
+		wg  *sync.WaitGroup
+	}
 }
 
 func NewStrMutex() *StrMutex {
-	return &StrMutex{strMutexes: make(map[string]*sync.Mutex)}
+	return &StrMutex{strMutexes: make(map[string]struct {
+		mtx *sync.Mutex
+		wg  *sync.WaitGroup
+	})}
 }
 
 func (m *StrMutex) Lock(str string) {
 	m.mapMutex.Lock()
 	strMutex, ok := m.strMutexes[str]
 	if !ok {
-		strMutex = new(sync.Mutex)
+		strMutex.mtx = new(sync.Mutex)
+		strMutex.wg = new(sync.WaitGroup)
 		m.strMutexes[str] = strMutex
 	}
 	m.mapMutex.Unlock()
 
-	strMutex.Lock()
+	strMutex.wg.Add(1)
+
+	// if this is the first Lock ever called, create a delete waiter
+	if !ok {
+		go func() {
+			strMutex.wg.Wait()
+
+			m.mapMutex.Lock()
+			delete(m.strMutexes, str)
+			m.mapMutex.Unlock()
+		}()
+	}
+
+	strMutex.mtx.Lock()
 }
 
 func (m *StrMutex) Unlock(str string) {
 	m.mapMutex.Lock()
 	strMutex, ok := m.strMutexes[str]
-	// delete(m.strMutexes, str) // TODO not thread safe!
 	m.mapMutex.Unlock()
 
 	if !ok {
 		panic(fmt.Sprintf("Unlock called for non locked string: %q", str))
 	}
 
-	strMutex.Unlock()
+	strMutex.mtx.Unlock()
+	strMutex.wg.Done()
 }
