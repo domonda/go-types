8a9c3902aa395b4f4bd6968e17dc650660d1c7cc
Erik Unger <erik@erikunger.com>
2020-01-04 17:54:38 +0100

    more implementations of the Validator and ValidatErr interfaces

--- validator.go
+++ validator.go
@@ -8,10 +8,82 @@ type Validator interface {
 	Valid() bool
 }
 
+// ValidatorFunc implements the Validator interface with a function.
+type ValidatorFunc func() bool
+
+// Valid returns if the data of the implementation is valid.
+func (f ValidatorFunc) Valid() bool {
+	return f()
+}
+
+// StaticValidator implements the Validator interface a bool validity value.
+type StaticValidator bool
+
+// Valid returns if the data of the implementation is valid.
+func (valid StaticValidator) Valid() bool {
+	return bool(valid)
+}
+
+type Validators []Validator
+
+// Valid returns if the data of the implementation is valid.
+func (v Validators) Valid() bool {
+	for _, validator := range v {
+		if !validator.Valid() {
+			return false
+		}
+	}
+	return true
+}
+
+// CombinedValidator creates a Validator whose Valid method
+// returns false if any of the passed validators Valid methods
+// returned false, else it returns true.
+func CombinedValidator(validators ...Validator) Validator {
+	return Validators(validators)
+}
+
 // ValidatErr can be implemented by types that can validate their data.
 type ValidatErr interface {
 	// Validate returns an error if the data of the implementation is not valid.
 	Validate() error
+}
+
+// ValidatErrFunc implements the ValidatErr interface with a function.
+type ValidatErrFunc func() error
+
+// Validate returns an error if the data of the implementation is not valid.
+func (f ValidatErrFunc) Validate() error {
+	return f()
+}
+
+// StaticValidatErr implements the ValidatErr interface for a validation error value.
+type StaticValidatErr struct {
+	Err error
+}
+
+// Validate returns an error if the data of the implementation is not valid.
+func (v StaticValidatErr) Validate() error {
+	return v.Err
+}
+
+type ValidatErrs []ValidatErr
+
+// Validate returns an error if the data of the implementation is not valid.
+func (v ValidatErrs) Validate() error {
+	for _, validatErr := range v {
+		if err := validatErr.Validate(); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// CombinedValidatErr creates a ValidatErr whose Validate method
+// returns the first error from the passed validatErrs Validate methods
+// or nil if none returned an error.
+func CombinedValidatErr(validatErrs ...ValidatErr) ValidatErr {
+	return ValidatErrs(validatErrs)
 }
 
 // ValidatorAsValidatErr wraps a Validator as a ValidatErr,
