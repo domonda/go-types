073c697434710ef38cbc9190176d7a5a2ff3de75
Erik Unger <erik@erikunger.com>
2020-04-17 14:12:22 +0200

    implement fmt.Stringer, json.Marshaler, json.Unmarshaler for nullable.Time

--- nullable/time.go
+++ nullable/time.go
@@ -1,15 +1,18 @@
 package nullable
 
 import (
+	"bytes"
 	"database/sql/driver"
+	"encoding/json"
 	"fmt"
 	"time"
 )
 
-// Time implements the sql.Scanner and driver.Valuer interfaces
-// and represents a time.Time where the zero time instant
+// Time represents a time.Time where the zero time instant
 // (which is the empty default value of the type)
-// is interpreted as SQL NULL.
+// is interpreted as SQL NULL and JSON null.
+// It implements the sql.Scanner and driver.Valuer interfaces
+// and also json.Marshaler and json.Unmarshaler.
 // It assumes that zero time instant is never used
 // in any real life application so it's free
 // to be used as magical value for representing NULL.
@@ -17,6 +20,14 @@ import (
 // instead of comparing with the default value of the type.
 type Time struct {
 	time.Time
+}
+
+// String returns Time.String() or "NULL" if Time.IsZero().
+func (nt Time) String() string {
+	if nt.Time.IsZero() {
+		return "NULL"
+	}
+	return nt.Time.String()
 }
 
 // Scan implements the database/sql.Scanner interface.
@@ -41,4 +52,22 @@ func (nt Time) Value() (driver.Value, error) {
 		return nil, nil
 	}
 	return nt.Time, nil
+}
+
+// UnarshalJSON implements encoding/json.Unmarshaler.
+// Interprets []byte(nil), []byte(""), []byte("null") as null.
+func (nt *Time) UnmarshalJSON(sourceJSON []byte) error {
+	if len(sourceJSON) == 0 || bytes.Equal(sourceJSON, []byte("null")) {
+		*nt = Time{}
+		return nil
+	}
+	return json.Unmarshal(sourceJSON, &nt.Time)
+}
+
+// MarshalJSON implements encoding/json.Marshaler
+func (nt Time) MarshalJSON() ([]byte, error) {
+	if nt.Time.IsZero() {
+		return []byte("null"), nil
+	}
+	return json.Marshal(nt.Time)
 }
