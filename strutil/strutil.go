bf06cd8d22454e99bbffebf1af9f496e1937d057
Erik Unger <erik@erikunger.com>
2022-03-16 08:51:17 +0100

    remove strutil.Cut, part of go 1.18 stdlib now

--- strutil/strutil.go
+++ strutil/strutil.go
@@ -601,17 +601,6 @@ func ToSnakeCase(s string) string {
 	return b.String()
 }
 
-// Cut slices s around the first instance of sep,
-// returning the text before and after sep.
-// The found result reports whether sep appears in s.
-// If sep does not appear in s, cut returns s, "", false.
-func Cut(s, sep string) (before, after string, found bool) {
-	if i := strings.Index(s, sep); i >= 0 {
-		return s[:i], s[i+len(sep):], true
-	}
-	return s, "", false
-}
-
 // CutTrimSpace slices s around the first instance of sep,
 // returning the text before and after sep with all leading
 // and trailing white space removed, as defined by Unicode.
