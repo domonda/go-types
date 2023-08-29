30f9974e0bc7625286e77d572c66d45d96accac8
Erik Unger <erik@erikunger.com>
2023-08-29 16:54:20 +0200

    strutil.ToSnakeCase handles whitespace and special characters

--- strutil/strutil.go
+++ strutil/strutil.go
@@ -627,11 +627,18 @@ func StringContainsAny(str string, subStrings []string) bool {
 // ToSnakeCase converts s to snake case
 // by lower casing everything and inserting '_'
 // before every new upper case character in s.
+// Whitespace, symbol, and punctuation characters
+// will be replace by '_'.
 func ToSnakeCase(s string) string {
 	var b strings.Builder
 	b.Grow(len(s) + 2)
 	lastWasUpper := true
 	for _, r := range s {
+		if unicode.IsSpace(r) || unicode.IsSymbol(r) || unicode.IsPunct(r) {
+			b.WriteByte('_')
+			lastWasUpper = true
+			continue
+		}
 		lr := unicode.ToLower(r)
 		isUpper := lr != r
 		if isUpper && !lastWasUpper {
